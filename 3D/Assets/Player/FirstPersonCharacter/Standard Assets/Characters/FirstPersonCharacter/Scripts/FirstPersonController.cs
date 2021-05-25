2d5cd260f2c79c38a4c28b2fee214d8688970135
himar33 <himar33@users.noreply.github.com>
2021-05-25 17:43:39 +0200

    uwu

--- 3D/Assets/Player/FirstPersonCharacter/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ 3D/Assets/Player/FirstPersonCharacter/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -12,8 +12,8 @@ namespace UnityStandardAssets.Characters.FirstPerson
     public class FirstPersonController : MonoBehaviour
     {
         [SerializeField] private bool m_IsWalking;
-        [SerializeField] private float m_WalkSpeed;
-        [SerializeField] private float m_RunSpeed;
+        [SerializeField] public float m_WalkSpeed;
+        [SerializeField] public float m_RunSpeed;
         [SerializeField] [Range(0f, 1f)] private float m_RunstepLenghten;
         [SerializeField] private float m_JumpSpeed;
         [SerializeField] private float m_StickToGroundForce;
@@ -25,7 +25,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
         [SerializeField] private CurveControlledBob m_HeadBob = new CurveControlledBob();
         [SerializeField] private LerpControlledBob m_JumpBob = new LerpControlledBob();
         [SerializeField] private float m_StepInterval;
-        [SerializeField] private AudioClip[] m_FootstepSounds;    // an array of footstep sounds that will be randomly selected from.
+        [SerializeField] public AudioClip[] m_FootstepSounds;    // an array of footstep sounds that will be randomly selected from.
         [SerializeField] private AudioClip m_JumpSound;           // the sound played when character leaves the ground.
         [SerializeField] private AudioClip m_LandSound;           // the sound played when character touches back on ground.
 
---
+++ 3D/Assets/Scripts/GroundBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class GroundBehaviour : MonoBehaviour
+{
+
+    public List<GroundType> groundType = new List<GroundType>();
+    public FirstPersonController fPC;
+    public string currentGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        setGroundType(groundType[0]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.transform.tag == "sand") setGroundType(groundType[0]);
+        else if (hit.transform.tag == "wood") setGroundType(groundType[1]);
+        else if (hit.transform.tag == "rock") setGroundType(groundType[2]);
+    }
+
+    public void setGroundType(GroundType ground)
+    {
+        fPC.m_FootstepSounds = ground.footStepSounds;
+        fPC.m_WalkSpeed = ground.walkSpeed;
+        fPC.m_RunSpeed = ground.runSpeed;
+        currentGround = ground.name;
+    }
+}
+
+[System.Serializable]
+public class GroundType
+{
+    public string name;
+    public AudioClip[] footStepSounds;
+    public float walkSpeed = 5;
+    public float runSpeed = 10;
+}
