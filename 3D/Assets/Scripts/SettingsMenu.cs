a998f7fc6e219061e8678ec61fac1e9e58195c96
himar33 <himbrgo@iesvalldemossa.net>
2021-05-30 23:11:10 +0200

    UWU THINGS

--- 3D/Assets/Scripts/SettingsMenu.cs
+++ 3D/Assets/Scripts/SettingsMenu.cs
@@ -1,18 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioMixer audioMixer;
+    public GameObject obj1;
+    public GameObject obj2;
+    public Button but1;
+    public Button but2;
+    public Button but3;
+
+    public TMPro.TMP_Dropdown resolutionDropdrown;
+    Resolution[] resolutions;
+
+    public void SetVolume(float volume)
     {
-        
+        audioMixer.SetFloat("volume", volume);
+    }
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
     }
 
-    // Update is called once per frame
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdrown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdrown.AddOptions(options);
+        resolutionDropdrown.value = currentResolutionIndex;
+        resolutionDropdrown.RefreshShownValue();
+    }
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            obj1.SetActive(true);
+            obj2.SetActive(false);
+            but1.interactable = true;
+            but2.interactable = true;
+            but3.interactable = true;
+        }
     }
 }
