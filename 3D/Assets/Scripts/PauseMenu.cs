3d7de0d05c38c78823ff03b0614fea5b9c88ef6a
himar33 <himbrgo@iesvalldemossa.net>
2021-05-31 17:01:49 +0200

    menu done

---
+++ 3D/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    public GameObject player;
+    public GameObject pauseMenuUI;
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if(gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        player.GetComponent<FirstPersonController>().enabled = true;
+        Cursor.visible = false;
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        player.GetComponent<FirstPersonController>().enabled = false;
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+        Cursor.visible = true;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loading menu...");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit game...");
+    }
+}
